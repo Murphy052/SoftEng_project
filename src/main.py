e2bb92c35583599cea62d8e7c1b215d6cf4eb942
Murphy052 <iguliyev@proton.me>
2024-11-21 18:58:42 +0400

    Disable Allowed Oringins

--- src/main.py
+++ src/main.py
@@ -22,7 +22,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(AuthenticationMiddleware, backend=BearerTokenAuthBackend())
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.ALLOWED_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
